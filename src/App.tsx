4bf20a8f5f064997194fd1fff15a565dc400c443
vadim <vadikoff57@gmail.com>
2020-02-21 19:38:32 +0300

    feat: add new features

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React, {useState} from 'react';
 import { Navbar } from "./components/Navbar";
 import{ Form} from "./components/form";
+import {TodoList} from './components/TodoList';
+import {IToDo} from './interfaces';
 function App() {
-  const [todos,setTodos] = useState([]);
+  const [todos,setTodos] = useState<Array<IToDo>>([]);
   const addHandler = (title:string) =>{
-    
+    const newTodo : IToDo = {
+      title: title,
+      id: Date.now(),
+      completed:false
+    }
+    setTodos(prev =>[newTodo,...todos])
   }
   return (
     <div className="wrapper">
     <Navbar/>
+    <div className="workbox">
     <Form onAdd = {addHandler}/>
+    <TodoList todos={todos}/>
+    </div>
     </div>
   ) 
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,17 +1,19 @@
 import React from "react";
+import { IToDo } from "../interfaces";
 type TodoListProps ={
-    todos:any[];
+    todos:IToDo[];
 }
 export const TodoList :React.FC<TodoListProps> = ({todos})=>{
     
     return(
         <ul className="todosbox">
             {todos.map((item)=>{
+
             return(
-            <li className="todos-item">
-                <label htmlFor="">
-                    <input type="checkbox"/>
-                    <span></span>
+            <li className="todos-item" key={item.id}>
+                <label className="todos-item-area" htmlFor="check">
+                    <input id="check" type="checkbox" checked={item.completed}/>
+                    <span>{item.title}</span>
                     <i className="material-icons red-text"></i>
                 </label>
             </li>
